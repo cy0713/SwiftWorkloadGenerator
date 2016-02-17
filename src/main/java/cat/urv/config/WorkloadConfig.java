e2bbdf4fdd723d3cc82462cd380d8daf5c247d40
raulgracia <raulgraciatinedo@gmail.com>
2016-02-17 17:28:14 +0100

    Deleted config values.

--- src/main/java/cat/urv/config/WorkloadConfig.java
+++ src/main/java/cat/urv/config/WorkloadConfig.java
@@ -4,19 +4,19 @@ public class WorkloadConfig {
 	
 	
 	//Swift configuration
-	public static final String tenant = "";
-	public static final String user = "";
-	public static final String password = "";
-	public static final String authURL = "";
-	public static final String container = "";
+	public static final String tenant = "spark-tenant";
+	public static final String user = "spark-user";
+	public static final String password = "spark-pass";
+	public static final String authURL = "http://iostack.urv.cat:5000/v2.0/tokens";
+	public static final String container = "data";
 	
 	//Point to the directory where the input workload trace is
-	public static final String inputWorkloadTrace = "";
+	public static final String inputWorkloadTrace = "/media/raul/Data/Documentos/Recerca/Publicaciones/2016/2016-middleware-iostack/datasets/Zeus_Documents/sorted.csv";
 	public static final String startTracePoint = "2015-09-09 10:00:00.000";
 	public static final String endTracePoint = "2015-09-09 10:00:59.000";
 			
 	//Point to the dataset characterizations directory
-	public static final String datasetCharacterizationsDir = "";
+	public static final String datasetCharacterizationsDir = "/media/raul/Data/Documentos/Recerca/Proyectos/IOStack/Code/WorkloadGen/characterizations/";
 	
 	//Temporal directory where file to be uploaded or downloaded are created
 	public static final String tmpFileDir =	"./";
