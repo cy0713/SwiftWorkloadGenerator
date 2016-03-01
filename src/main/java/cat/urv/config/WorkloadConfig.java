2f161834c56749ce27f4f12941a8f8f7d3fa42a7
raulgracia <raulgraciatinedo@gmail.com>
2016-03-01 17:25:38 +0100

    Clean config file.

--- src/main/java/cat/urv/config/WorkloadConfig.java
+++ src/main/java/cat/urv/config/WorkloadConfig.java
@@ -4,20 +4,20 @@ public class WorkloadConfig {
 	
 	
 	//Swift configuration
-	public static final String tenant = "spark-tenant";
-	public static final String user = "spark-user";
-	public static final String password = "spark-pass";
-	public static final String authURL = "http://iostack.urv.cat:5000/v2.0/tokens";
-	public static final String container = "data";
+	public static final String tenant = "";
+	public static final String user = "";
+	public static final String password = "";
+	public static final String authURL = "";
+	public static final String container = "";
 	
 	//Point to the directory where the input workload trace is
-	public static final String inputWorkloadTrace = "/media/raul/Data/Documentos/Recerca/Publicaciones/2016/2016-middleware-iostack/datasets/arctur.csv";
+	public static final String inputWorkloadTrace = "";
 			//Zeus_Documents/sorted.csv";
 	public static final String startTracePoint = "2016-01-11 14:31:38"; //"2015-09-09 10:00:00.000";
 	public static final String endTracePoint = "2016-01-11 14:32:38"; // "2015-09-09 10:00:59.000";
 			
 	//Point to the dataset characterizations directory
-	public static final String datasetCharacterizationsDir = "/media/raul/Data/Documentos/Recerca/Proyectos/IOStack/Code/WorkloadGen/characterizations/";
+	public static final String datasetCharacterizationsDir = "./characterizations/";
 	
 	//Temporal directory where file to be uploaded or downloaded are created
 	public static final String tmpFileDir =	"./";
