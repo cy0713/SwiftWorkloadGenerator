404b7b95d29845e256d2a4f7e6dddc829953e14e
RaulGracia <raulgraciatinedo@gmail.com>
2016-04-05 15:38:43 +0200

    Simple workload for BW experiments.

--- src/main/java/cat/urv/workloadgen/execution/Executor.java
+++ src/main/java/cat/urv/workloadgen/execution/Executor.java
@@ -9,7 +9,7 @@ import static cat.urv.config.WorkloadConfig.container;
 public class Executor {
     public static void main( String[] args ){	
     	System.out.println("--------- Starting Workload!! -----------");
-    	WorkloadExecutor executor = new ArcturTraceWorkloadExecutor(); // SinusoidalWorkloadExecutor();
+    	WorkloadExecutor executor = new SimpleWorkloadExecutor(); //ArcturTraceWorkloadExecutor(); // SinusoidalWorkloadExecutor();
     	executor.executeWorkload(container);
     	System.out.println("--------- Finishing Workload!! -----------");
     	System.exit(0);
--- src/main/java/cat/urv/workloadgen/execution/SimpleWorkloadExecutor.java
+++ src/main/java/cat/urv/workloadgen/execution/SimpleWorkloadExecutor.java
@@ -12,23 +12,53 @@ import cat.urv.workloadgen.generation.WorkloadWorkerPool;
 
 public class SimpleWorkloadExecutor extends WorkloadExecutor {
 	
+	static int BLOCK_SIZE = 32*1024*1024;
+	
+	static int INITIAL_PUTS = 5;
+	
+	static int READS = 50;
+	
 	public void executeWorkload(String targetContainer) {		
 		WorkloadWorkerPool pool = new WorkloadWorkerPool();
 		Container container = new SwiftAPI().getSwiftContainer(targetContainer);		
 		pool.initialize(container);
-		for (int i = 0; i<10; i++){
+		//First, initialize the container with several objects (PUT)
+		System.out.println("Initializing containers with objects");
+		for (int i = 0; i<INITIAL_PUTS; i++){
 			WorkloadTask t = new WorkloadTask();
 			t.setDataType(DataTypes.DOCS);
 			t.setId(new Random().nextLong());
 			t.setOperationType(OperationType.WRITE);
-			t.setSize(32*1024);
+			t.setSize(BLOCK_SIZE);
+			pool.ingestJob(t);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Writing file " + i);
+		}
+		
+		try {
+			System.out.println("PUTs done, starting reads");
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		//Second, do read-oriented workload
+		for (int i = 0; i<READS; i++){
+			WorkloadTask t = new WorkloadTask();
+			t.setDataType(DataTypes.DOCS);
+			t.setId(new Random().nextLong());
+			t.setOperationType(OperationType.READ);
 			pool.ingestJob(t);
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.println("Writing file " + i);
+			System.out.println("Reading file " + i);
 		}
 		
 		pool.tearDown();
